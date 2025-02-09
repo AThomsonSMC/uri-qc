888c250cc3584434722674249da843f3fec809f5
Adam Thomson <athomson@biobot.io>
2025-02-09 13:23:13 -0700

    more gg progress

---
+++ genomic_grover/constants.py
@@ -0,0 +1,15 @@
+REFERENCE_GENOMES = {
+    'test0': "GCAT",
+    'test1': "GATACA",
+    'test2': "AACCGGTT",
+    'test3': "ACGTACGTACGTACGTACGTACGTACGTACGT",
+    'test4': "TGCATGCATGCATGCA",
+    'test5': "GACATCAGGGATCCAT"
+}
+
+BASE_MAP = {
+    "A": "00",
+    "C": "01",
+    "G": "10",
+    "T": "11",
+}
